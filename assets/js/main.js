84a73531994e6233646446f2c54722115bac52d1
Vadislav Zhuk <malcolm.madsheep@gmail.com>
2015-11-22 15:18:18 +0200

    wheel scrolling refactoring done

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,15 @@ $(function() {
 		againBtn = $('#againBtn'),
 		toNextPictureButton = $('#nextPic'),
 		toPrevPictruveButton = $('#prevPic'),
-		collectionItems = $('.collection-item'),
-		imageFailLoadingBox = $('#imgloadingfail');
+		collectionItems = $('.collection-item').children('img'),
+		imageFailLoadingBox = $('#imgloadingfail'),
+		isScrolling = false;
 
 	setCollectionListWidth();
 	complexity.on('change', setLevel);
 	files.on('change', handleFileUploading);
-	
-	previewImage.on('load', handlePreviewImageSuccessLoading);
+
+	previewImage.on('load', handlePreviewImageSuccessfulLoading );
 	previewImage.on('error', handlePreviewImageErrorLoading);
 
 	window.addEventListener('keydown', puzzle.handleKeyInput.bind(puzzle), false);
@@ -48,88 +49,96 @@ $(function() {
 	$(collectionItems[0]).click();
 
 	function selectItem(evt) {
-		var currentItemId = getSelectedItemId(),
-			t = evt.target,
-			src = t.src,
-			tWidth = collectionItemsList.outerWidth(),
-			aWidth = colil.outerWidth(),
-			isEnd = false;
+		var newItem = evt.target;
+
 		deselectCollectionItems();
-		// if (t.tagName.toLov === 'li') {
-		// 	t = $(t).children('img');
+		makeSelected(newItem);
+
+
+		// console.log(newItem);
+		// var previousSelectedId = getSelectedItemId(),
+		// 	t = evt.target,
+		// 	src = t.src,
+		// 	tWidth = collectionItemsList.outerWidth(),
+		// 	aWidth = colil.outerWidth(),
+		// 	isEnd = false,
+		// 	prevOffset = $(collectionItems[previousSelectedId]).offsetLeft;
+
+		// deselectCollectionItems();
+		// $(t).addClass('selected-item');
+		// var newSelectedId = getSelectedItemId(),
+		// 	newOffset = t.offsetLeft;
+
+		// 	console.log('no', previousSelectedId);
+		// 	console.log('po', prevOffset);
+		// if (id !== 0) {
+		// 	prevOffset = $(collectionItems[id - 1]).get(0).offsetLeft
 		// }
-		console.log(t);
-		$(t).addClass('selected-item');
-		var id = getSelectedItemId(),
-			prevOffset = $(collectionItems[id]).get(0).offsetLeft;
-		if (id !== 0) {
-			prevOffset = $(collectionItems[id - 1]).get(0).offsetLeft
-		}
 
-		previewImage.prop('src', src);
+		// previewImage.prop('src', src);
 
-		if (tWidth - prevOffset < aWidth) {
-			isEnd = true;
-		}
-		// console.log(tWidth, '-', prevOffset, '=', aWidth);
+		// if (tWidth - prevOffset < aWidth) {
+		// 	isEnd = true;
+		// }
+		// // console.log(tWidth, '-', prevOffset, '=', aWidth);
 
-		if (!isEnd) {
-			collectionItemsList.animate({
-				'left': -prevOffset
-			}, 500);
-		}
-
+		// if (!isEnd) {
+		// 	collectionItemsList.animate({
+		// 		'left': -prevOffset
+		// 	}, 500);
+		// }
+		previewImage.prop('src', $(newItem).prop('src'));
 	}
 
 	function changeSelectedItem(evt) {
-		var currentItemId = getSelectedItemId(),
-			target = evt.target,
-			left = parseInt(collectionItemsList.css('left')),
-			delta = 0,
-			isStart = (currentItemId === 0) ? true : false,
-			tWidth = collectionItemsList.outerWidth(),
-			aWidth = colil.outerWidth(),
-			isEnd = false;
-		if (target.tagName.toLowerCase() === 'span') {
-			target = $(target).parents('div').get(0);
-		}
+		// var currentItemId = getSelectedItemId(),
+		// 	target = evt.target,
+		// 	left = parseInt(collectionItemsList.css('left')),
+		// 	delta = 0,
+		// 	isStart = (currentItemId === 0) ? true : false,
+		// 	tWidth = collectionItemsList.outerWidth(),
+		// 	aWidth = colil.outerWidth(),
+		// 	isEnd = false;
+		// if (target.tagName.toLowerCase() === 'span') {
+		// 	target = $(target).parents('div').get(0);
+		// }
 
-		var item = $(collectionItems[currentItemId]),
-			delta = 0,
-			prevOffset = item.get(0).offsetLeft;
-		// console.log(isStart);
-		item.removeClass('selected-item');
-		console.log(currentItemId);
-		if (target.id === 'nextPic') {// && id + 1 < collectionItems.length) {
-			// id++;
-			selectNextItem(currentItemId);
-		} else if (target.id === 'prevPic') { //} && id > 0) {
-			// prevOffset = $(collectionItems[id - 1]).get(0).offsetLeft;
-			// id--;
-			// if (id !== 0) {
-			// 	prevOffset = $(collectionItems[id - 1]).get(0).offsetLeft;
-			// }
-			selectPreviousItem(currentItemId);
-		}
-		// console.log('offset left', item.get(0).offsetLeft);
-		// console.log(tWidth - prevOffset, '=', aWidth);
-		if (tWidth - prevOffset < aWidth) {
-			isEnd = true;
-		}
+		// var item = $(collectionItems[currentItemId]),
+		// 	delta = 0,
+		// 	prevOffset = item.get(0).offsetLeft;
+		// // console.log(isStart);
+		// item.removeClass('selected-item');
+		// console.log(currentItemId);
+		// if (target.id === 'nextPic') {// && id + 1 < collectionItems.length) {
+		// 	// id++;
+		// 	selectNextItem(currentItemId);
+		// } else if (target.id === 'prevPic') { //} && id > 0) {
+		// 	// prevOffset = $(collectionItems[id - 1]).get(0).offsetLeft;
+		// 	// id--;
+		// 	// if (id !== 0) {
+		// 	// 	prevOffset = $(collectionItems[id - 1]).get(0).offsetLeft;
+		// 	// }
+		// 	selectPreviousItem(currentItemId);
+		// }
+		// // console.log('offset left', item.get(0).offsetLeft);
+		// // console.log(tWidth - prevOffset, '=', aWidth);
+		// if (tWidth - prevOffset < aWidth) {
+		// 	isEnd = true;
+		// }
 
-		item = $(collectionItems[currentItemId]);
-		var offsetLeft = item.get(0).offsetLeft,
-			src = item.prop('src'),
-			tX = item.get(0).x;
+		// item = $(collectionItems[currentItemId]);
+		// var offsetLeft = item.get(0).offsetLeft,
+		// 	src = item.prop('src'),
+		// 	tX = item.get(0).x;
 
-		$(collectionItems[currentItemId]).addClass('selected-item');
-		previewImage.prop('src', src);
-		if (!isStart && !isEnd) {
-			// console.log('wtf');
-			collectionItemsList.animate({
-				'left': -prevOffset
-			}, 500);
-		}
+		// $(collectionItems[currentItemId]).addClass('selected-item');
+		// previewImage.prop('src', src);
+		// if (!isStart && !isEnd) {
+		// 	// console.log('wtf');
+		// 	collectionItemsList.animate({
+		// 		'left': -prevOffset
+		// 	}, 500);
+		// }
 	}
 
 	// return new offset
@@ -174,29 +183,37 @@ $(function() {
 	}
 
 	function slideCollection(evt) {
-		var original = evt.originalEvent,
+		var deltaY = evt.originalEvent.deltaY,
 			target = evt.target,
 			type = evt.type,
 			left = parseInt(collectionItemsList.css('left')),
-			delta = 15,
+			delta = 150,
 			tWidth = colil.width();
 
-		if (target.localName === 'span') {
-			target = $(target).parents('div').get(0);
-		}
-
-		if (type !== 'wheel') {
-			delta = 50;
-		}
-
-		if (original.deltaY < 0 && type === 'wheel' && left < 0) {
+		if (deltaY < 0 && left < 0) {
 			left += delta;
-		} else if ((original.deltaY > 0 && type === 'wheel')) {
-			if (collWidth - Math.abs(left) > tWidth) {
-				left -= delta;
+			if (left >= 0) {
+				left = 0;
+			}
+		} else if (deltaY > 0 && (collWidth - Math.abs(left) > tWidth)) {
+			left -= delta;
+			if (Math.abs(left) > tWidth) {
+				left = tWidth - collWidth;
 			}
 		}
-		collectionItemsList.css('left', left);
+
+		scrollItemList(left);
+	}
+
+	function scrollItemList(offset) {
+		if (!isScrolling) {
+			isScrolling = true;
+			collectionItemsList.animate({
+				'left': offset
+			}, 300, function() {
+				isScrolling = false;
+			});
+		}
 	}
 
 	function setLevel(evt) {
@@ -217,7 +234,7 @@ $(function() {
 		}
 	}
 
-	
+
 
 	function handleLoadingImageFromURL(evt) {
 		var source = imageURLBox.val().trim();
@@ -257,8 +274,8 @@ $(function() {
 
 	}
 
-	function handlePreviewImageSuccessLoading(evt) {
-		var target  = $(evt.target);
+	function handlePreviewImageSuccessfulLoading(evt) {
+		var target = $(evt.target);
 		if (!target.hasClass('borderedpi')) {
 			target.addClass('borderedpi');
 		}
@@ -289,5 +306,9 @@ $(function() {
 				return i;
 			}
 		}
+	}
+
+	function makeSelected(item) {
+		$(item).addClass('selected-item');
 	}
 });
